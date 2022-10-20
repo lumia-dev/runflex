22c746b533729d2dd744eb93e1330bda6bd5850b
Guillaume Monteil <gmonteil@ik.me>
2022-10-20 12:59:04 +0200

    Implementation of a "tmp" resolver for the config files: creates a temporary directory, which will be deleted when the program exits (+ example use in the defaults.yaml file).

--- runflex/config.py
+++ runflex/config.py
@@ -5,9 +5,11 @@ from runflex import prefix
 from runflex.utilities import getfile
 from runflex.archive import Rclone
 from pandas import Timedelta
+import tempfile
 
 
 OmegaConf.register_new_resolver("prefix", lambda x: prefix.joinpath(x))
 OmegaConf.register_new_resolver("file", lambda x: getfile(x))
 OmegaConf.register_new_resolver("rclone", lambda x: Rclone(x))
 OmegaConf.register_new_resolver('dt', lambda x: Timedelta(x))
+OmegaConf.register_new_resolver('tmp', lambda x: tempfile.TemporaryDirectory(prefix='runflex', dir=x).name)
